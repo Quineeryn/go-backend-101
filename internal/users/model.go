ce360d7e17382cba033c94f6c6ec5595878fa19b
Quineeryn <canandraeka10@gmail.com>
2025-09-04 18:58:27 +0700

    update: Sign tokens with user role; add users.Store.SetRole helper

--- internal/users/model.go
+++ internal/users/model.go
@@ -6,6 +6,7 @@ type User struct {
 	ID           string    `json:"id" gorm:"primaryKey"`
 	Name         string    `json:"name"`
 	Email        string    `json:"email" gorm:"column:email"`
+	Role         string    `json:"role" gorm:"default:user"`
 	PasswordHash *string   `json:"-" gorm:"column:password_hash"`
 	CreatedAt    time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt    time.Time `json:"updated_at" gorm:"autoUpdateTime"`
